4a6cdde612e2b09383595a2ee8c383677d90f7b9
joonsoo.yang <didwns7347@gmail.com>
2024-11-25 17:04:24 +0900

    update: entity 작업 완료

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,18 +16,36 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
-
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderitems = new ArrayList();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 메소드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public  void addOrderItem(OrderItem orderItem) {
+        orderitems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@DiscriminatorValue("M")
+@DiscriminatorValue("A")
 public class Album  extends Item {
     String artist;
     String etc;
